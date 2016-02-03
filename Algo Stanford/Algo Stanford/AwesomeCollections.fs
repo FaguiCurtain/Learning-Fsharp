4366b3eedde91a9099de7339911155f0ea390a74
FaguiCurtain <fgrideau@gmail.com>
2016-02-03 23:19:55 +0900

    Dijkstra still bugged

---
+++ Algo Stanford/Algo Stanford/AwesomeCollections.fs
@@ -0,0 +1,189 @@
+﻿(* AwesomeCollections.fs *)
+namespace AwesomeCollections
+
+type 'a stack =
+    | EmptyStack
+    | StackNode of 'a * 'a stack
+        
+module Stack =        
+    let hd = function
+        | EmptyStack -> failwith "Empty stack"
+        | StackNode(hd, tl) -> hd
+        
+    let tl = function
+        | EmptyStack -> failwith "Emtpy stack"
+        | StackNode(hd, tl) -> tl
+        
+    let cons hd tl = StackNode(hd, tl)
+    
+    let empty = EmptyStack
+        
+    let rec rev s =
+        let rec loop acc = function
+            | EmptyStack -> acc
+            | StackNode(hd, tl) -> loop (StackNode(hd, acc)) tl
+        loop EmptyStack s
+
+type Queue<'a>(f : stack<'a>, r : stack<'a>) =
+    let check = function
+        | EmptyStack, r -> Queue(Stack.rev r, EmptyStack)
+        | f, r -> Queue(f, r)
+
+    member this.hd =
+        match f with
+        | EmptyStack -> failwith "empty"
+        | StackNode(hd, tl) -> hd
+        
+    member this.tl =
+        match f, r with
+        | EmptyStack, _ -> failwith "empty"
+        | StackNode(x, f), r -> check(f, r)
+        
+    member this.enqueue(x) = check(f, StackNode(x, r))
+    
+    static member empty = Queue<'a>(Stack.empty, Stack.empty)
+
+type color = R | B    
+type 'a tree =
+    | E
+    | T of color * 'a tree * 'a * 'a tree
+    
+module Tree =
+    let hd = function
+        | E -> failwith "empty"
+        | T(c, l, x, r) -> x
+        
+    let left = function
+        | E -> failwith "empty"
+        | T(c, l, x, r) -> l
+    
+    let right = function
+        | E -> failwith "empty"
+        | T(c, l, x, r) -> r
+        
+    let rec exists item = function
+        | E -> false
+        | T(c, l, x, r) ->
+            if item = x then true
+            elif item < x then exists item l
+            else exists item r
+            
+    let balance = function                              (* Red nodes in relation to black root *)
+        | B, T(R, T(R, a, x, b), y, c), z, d            (* Left, left *)
+        | B, T(R, a, x, T(R, b, y, c)), z, d            (* Left, right *)
+        | B, a, x, T(R, T(R, b, y, c), z, d)            (* Right, left *)
+        | B, a, x, T(R, b, y, T(R, c, z, d))            (* Right, right *)
+            -> T(R, T(B, a, x, b), y, T(B, c, z, d))
+        | c, l, x, r -> T(c, l, x, r)
+    
+    let insert item tree =
+        let rec ins = function
+            | E -> T(R, E, item, E)
+            | T(c, a, y, b) as node ->
+                if item = y then node
+                elif item < y then balance(c, ins a, y, b)
+                else balance(c, a, y, ins b)
+
+        (* Forcing root node to be black *)                
+        match ins tree with
+            | E -> failwith "Should never return empty from an insert"
+            | T(_, l, x, r) -> T(B, l, x, r)
+            
+    let rec print (spaces : int) = function
+        | E -> ()
+        | T(c, l, x, r) ->
+            print (spaces + 4) r
+            printfn "%s %A%A" (new System.String(' ', spaces)) c x
+            print (spaces + 4) l
+    
+type BinaryTree<'a when 'a : comparison> (inner : 'a tree) =
+    member this.hd = Tree.hd inner
+    member this.left = BinaryTree(Tree.left inner)
+    member this.right = BinaryTree(Tree.right inner)
+    member this.exists item = Tree.exists item inner
+    member this.insert item = BinaryTree(Tree.insert item inner)
+    member this.print() = Tree.print 0 inner
+    static member empty = BinaryTree<'a>(E)
+
+type 'a heap =
+    | EmptyHeap
+    | HeapNode of int * 'a * 'a heap * 'a heap
+ 
+module Heap =
+    let height = function
+        | EmptyHeap -> 0
+        | HeapNode(h, _, _, _) -> h
+ 
+    (* Helper function to restore the leftist property *)        
+    let makeT (x, a, b) =
+        if height a >= height b then HeapNode(height b + 1, x, a, b)
+        else HeapNode(height a + 1, x, b, a)
+ 
+    let rec merge comparer = function
+        | x, EmptyHeap -> x
+        | EmptyHeap, x -> x
+        | (HeapNode(_, x, l1, r1) as h1), (HeapNode(_, y, l2, r2) as h2) ->
+            if comparer x y <= 0 then makeT(x, l1, merge comparer (r1, h2))
+            else makeT (y, l2, merge comparer (h1, r2))
+ 
+    let hd = function
+        | EmptyHeap -> failwith "empty"
+        | HeapNode(h, x, l, r) -> x
+ 
+    let tl comparer = function
+        | EmptyHeap -> failwith "empty"
+        | HeapNode(h, x, l, r) -> merge comparer (l, r)
+        
+    let rec to_seq comparer = function
+        | EmptyHeap -> Seq.empty
+        | HeapNode(h, x, l, r) as node -> seq { yield x; yield! to_seq comparer (tl comparer node) }
+ 
+type 'a BinaryHeap(comparer : 'a -> 'a -> int, inner : 'a heap) =
+    (* private *)
+    member this.inner = inner
+ 
+    (* public *)
+    member this.hd = Heap.hd inner
+    member this.tl = BinaryHeap(comparer, Heap.tl comparer inner)
+    member this.merge (other : BinaryHeap<_>) = BinaryHeap(comparer, Heap.merge comparer (inner, other.inner))
+    member this.insert x = BinaryHeap(comparer, Heap.merge comparer (inner,(HeapNode(1, x, EmptyHeap, EmptyHeap))))
+    
+    interface System.Collections.Generic.IEnumerable<'a> with
+        member this.GetEnumerator() = (Heap.to_seq comparer inner).GetEnumerator()
+            
+    interface System.Collections.IEnumerable with
+        member this.GetEnumerator() = (Heap.to_seq comparer inner :> System.Collections.IEnumerable).GetEnumerator()
+ 
+    static member make(comparer) = BinaryHeap<_>(comparer, EmptyHeap)
+
+type 'a lazyStack =
+    | Node of Lazy<'a * 'a lazyStack>
+    | EmptyStack
+ 
+module LazyStack =
+    let (|Cons|Nil|) = function
+        | Node(item) ->
+            let hd, tl = item.Force()
+            Cons(hd, tl)
+        | EmptyStack -> Nil
+ 
+    let hd = function
+        | Cons(hd, tl) -> hd
+        | Nil -> failwith "empty"
+ 
+    let tl = function
+        | Cons(hd, tl) -> tl
+        | Nil -> failwith "empty"
+ 
+    let cons(hd, tl) = Node(lazy(hd, tl))
+    
+    let empty = EmptyStack
+ 
+    let rec append x y =
+        match x with
+        | Cons(hd, tl) -> Node(lazy(printfn "appending... got %A" hd; hd, append tl y))
+        | Nil -> y
+ 
+    let rec iter f = function
+        | Cons(hd, tl) -> f(hd); iter f tl
+        | Nil -> ()
---
+++ Algo Stanford/Algo Stanford/Djikstra1.fs
@@ -0,0 +1,147 @@
+﻿///////////////// preparing the data ////////////////////
+
+open System
+open System.Collections.Generic
+open System.IO
+
+open MSDN.FSharp
+
+let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+
+let x = File.ReadAllLines "C:\Users\Fagui\Documents\GitHub\Learning Fsharp\Algo Stanford\PA 5 - dijkstraData.txt"
+// let x = File.ReadAllLines "C:\Users\Fagui\Documents\GitHub\Learning Fsharp\Algo Stanford\PA5 - test4.txt"
+// val x : string [] =
+
+// original format of each row is "row_number (a1,b1) (a2,b2) ....(an,bn)"
+
+let split (text:string)=
+    text.Split [|'\t';' '|]
+
+let split_comma (text:string)=
+    text.Split [|','|]
+
+
+let splitIntoKeyValue (A: 'T[]) =  
+    (A.[0], Seq.toList (Seq.tail A))
+
+let parseLine (line:string)=
+    line
+    |> split
+    |> Array.filter (fun s -> not(s=""))
+  //  |> Array.map (fun s-> (int s))
+    |> splitIntoKeyValue
+
+let y =
+  x |> Array.map parseLine
+
+let nodelist = y |> Array.map fst |> Array.map int
+
+let N1 = Array.max nodelist // attention si il y a un node image plus grand encore !!!
+
+
+let graphcore = // (int*int) list [] // il va manquer les nodes qui n'ont pas de chemins sortants
+    (y |> Array.map snd
+       |> Array.map (List.map split_comma)
+       |> Array.map (List.map (fun (A:'T[]) -> (int A.[0],int A.[1]) )))
+                  
+
+let N2 = graphcore |> Array.map (List.map fst) |> Array.map List.max |> Array.max // node max
+
+let N=N2
+
+// construction non optimisée, perte de temps
+// rajoute un élément pour 0 dont on ne se sert pas           
+let graph = 
+    let g = Array.create (N+1) []
+    for i in 0..((Array.length nodelist)-1) do
+        g.[nodelist.[i]] <- graphcore.[i]
+    g
+
+
+let reversegraph = // (int*int) list []
+    let (rg:(int*int) list [])= Array.create (N+1) []
+    for i in 1..N do
+        graph.[i] |> List.iter (fun (node,value) -> rg.[node] <- (i,value)::rg.[node] )
+    rg
+        
+/////////////////// test ///////////////////
+
+//let PQ1 = new PriorityQueue<int,int>()
+//
+//PQ1.Enqueue 2 3
+//PQ1.Enqueue 3 4
+//PQ1.Enqueue 1 5
+//PQ1.Enqueue 1 6
+//printfn "PriorityQueue %A" PQ1
+//
+//Console.ReadKey() |> ignore
+
+/////////////////// DJIKSTRA ///////////////////
+let limit = 1000000 // max distance limit
+let S = 1 // Source
+let V = [0..N] |> List.filter (fun s -> not(s=S));;
+let A = Array.create (N+1) limit // on ne se sert pas de A.[0]
+A.[S] <- 0
+
+let C = Array.create (N+1) -1 // enregistre l'index de l'élément de X le plus proche d'un élément de V.
+let D = Array.create (N+1) limit // enregistre le critère de Djikstra
+
+let inX = Array.create (N+1) false // enregistre si le node est dans X, cad a été déjà traité
+inX.[S]<-true
+
+let PQ = new PriorityQueue<int,int>() // Key = distance to X ; Value = Node 
+
+let init_loop () : unit =
+    for node in V do
+        PQ.Enqueue limit node 
+    for (node,dist_to_S) in graph.[S] do
+         PQ.RemoveAt (PQ.IndexOf limit node) |> ignore
+         PQ.Enqueue dist_to_S node |> ignore
+         C.[node]<-S
+         D.[node]<-dist_to_S
+init_loop()
+
+let PP () =
+    for i in 0..(PQ.Count-1) do (printfn "PQ %i %A" i PQ.[i]);;
+    
+let one_loop() : int =
+    // take the first element from the queue
+    let z = PQ.Dequeue()
+    let W = z.Value
+    let l = z.Key
+    A.[W]<- l
+    // maintain the heap
+    // the Key must the Dijkstra criterion
+    let update_list = graph.[W]
+    update_list |> List.iter ( fun (node,dist) -> if (inX.[node]=true) then ()
+                                                     else let x = l+dist                                                    
+                                                          if x > D.[node] then ()
+                                                              else PQ.RemoveAt (PQ.IndexOf D.[node] node) |> ignore // updater le node
+                                                                   PQ.Enqueue x node
+                                                                   C.[node]<- W // prédécesseur
+                                                                   D.[node]<- x // update la distance à X
+                                                                   if node = 196 then printfn "D.[196] = %d" x else ()
+                              ) 
+    inX.[W] <- true
+    W
+
+for k in 1..N do // one_loop()
+                 printfn "k= %d W=%d" k (one_loop())
+                                          
+printfn "%A" A
+// printfn "%i,%i,%i,%i,%i,%i,%i,%i,%i,%i" A.[7] A.[37] A.[59] A.[82] A.[99] A.[115] A.[133] A.[165] A.[188] A.[197]
+PP()
+
+stopWatch.Stop()
+printfn "%f" stopWatch.Elapsed.TotalMilliseconds
+Console.ReadKey() |> ignore
+
+// 1er essai 2599,2610,2947,2052,2367,2399,2029,2442,2610,3068//
+// la bonne réponse pour A.[188] devrait être 2505...
+
+// solution venant de Python
+let AA =[|1000000;0; 2971; 2644; 3056; 2525; 2818; 2599; 1875; 745; 3205; 1551; 2906; 2394; 1803; 2942; 1837; 3111; 2284; 1044; 2351; 3630; 4028; 2650; 3653; 2249; 2150; 1222; 2090; 3540; 2303; 3455; 3004; 2551; 2656; 998; 2236; 2610; 3548; 1851; 4091; 2732; 2040; 3312; 2142; 3438; 2937; 2979; 2757; 2437; 3152; 2503; 2817; 2420; 3369; 2862; 2609; 2857; 3668; 2947; 2592; 1676; 2573; 2498; 2047; 826; 3393; 2535; 4636; 3650; 743; 1265; 1539; 3007; 4286; 2720; 3220; 2298; 2795; 2806; 982; 2976; 2052; 3997; 2656; 1193; 2461; 1608; 3046; 3261; 2018; 2786; 647; 3542; 3415; 2186; 2398; 4248; 3515; 2367; 2970; 3536; 2478; 1826; 2551; 3368; 2303; 2540; 1169; 3140; 2317; 2535; 1759; 1899; 508; 2399; 3513; 2597; 2176; 1090; 2328; 2818; 1306; 2805; 2057; 2618; 1694; 3285; 1203; 676; 1820; 1445; 2468; 2029; 1257; 1533; 2417; 3599; 2494; 4101; 546; 1889; 2616; 2141; 2359; 648; 2682; 3464; 2873; 3109; 2183; 4159; 1832; 2080; 1831; 2001; 3013; 2143; 1376; 1627; 2403; 4772; 2556; 2124; 1693; 2442; 3814; 2630; 2038; 2776; 1365; 3929; 1990; 2069; 3558; 1432; 2279; 3829; 2435; 3691; 3027; 2345; 3807; 2145; 2703; 2884; 3806; 1151; 2505; 2340; 2596; 4123; 1737; 3136; 1073; 1707; 2417; 3068; 1724; 815; 2060|]
+
+let B = [|for i in 0..200 do yield A.[i]-AA.[i]|];;
+let BB = [0..N] |> List.filter (fun s-> not(B.[s]=0))
+//val BB : int list = [10; 26; 95; 96; 101; 147; 157; 184; 188; 196]// différences
